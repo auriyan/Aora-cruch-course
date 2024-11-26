446572227adbaecf123ef6940e14a81993ee061f
auriyan <auriyan89@gmail.com>
2024-11-26 21:40:04 +0200

    Initial commit

---
+++ app/index.jsx
@@ -0,0 +1,34 @@
+import { StatusBar } from 'expo-status-bar';
+import { Image, ScrollView, Text, View } from 'react-native';
+import {Link} from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import {images} from '../constants';
+
+export default function app(){
+    return(
+        <SafeAreaView className="bg-primary h-full">
+            <ScrollView contentContainerStyle={{height : '100%'}}>
+                <View className="w-full justify-center items-center h-full px-4">
+                    <Image
+                        source={images.logo}
+                        className="w-[130px] h-[84px]"
+                        resizeMode="contain"
+                    />
+
+                    <Image 
+                        source={images.cards}
+                        className="max-w--[380px] w-full h-[300px]"
+                        resizeMode="contain"
+                    />
+
+                    <View className="relative mt-5">
+                        <Text className = "text-3xl text-white font-bold text-canter">
+                            
+                        </Text>
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
+}
